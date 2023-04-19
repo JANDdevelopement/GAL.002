a277a96cc739fb16cbb12ecf0e7d0e1bda42b39f
MRGalaxy <108481498+GamingGalaxy200@users.noreply.github.com>
2023-04-20 00:05:08 +0200

    OnStream Update
    A Error with the .env file was fixxed.
    New partials were added.
    Asked list (line 31 index) created.
    Bot now answers in DM without TAG.
    In servers still only with @.
    Console log and personalities updated.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import dotenv from "dotenv";
-import { Client, GatewayIntentBits } from "discord.js";
+import { Client, GatewayIntentBits, Partials } from "discord.js";
 import { Configuration, OpenAIApi } from "openai";
 
 dotenv.config();
 
 const client = new Client({
+    partials: [Partials.Channel, Partials.User, Partials.Message, Partials.Reaction, Partials.GuildMember],
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
@@ -13,6 +14,7 @@ const client = new Client({
         GatewayIntentBits.DirectMessages,
     ],
 });
+
 
 const openai = new OpenAIApi(new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
@@ -26,12 +28,41 @@ client.on('ready', () => {
     .catch(console.error);
 });
 
+const asked = [ "1091849328924037272", "Eintrag 2", "Eintrag 3" ]
+asked.push("Neuer Eintrag")
+
 client.on("messageCreate", async function (message) {
+    if (message.author.bot) {
+      console.log(`${message.author.username}: ${message.content}`)
+
+    }
     if (message.author.bot) return;
-    console.log(`MSG - ${message.content}`)
-    if (!message.content.startsWith(`<@${process.env.BOT_ID}>`)) return;
-    
-    try {
+    console.log(`${message.author.username} - ${message.content}`)
+    // Antwort auf DMs
+    if (message.guild === null) {
+      try {
+        const response = await openai.createChatCompletion({
+          model: "gpt-3.5-turbo",
+          messages: [
+            {role: "system", content: "You are a helpful german assistant who responds succinctly on german if hes not asked to respond on another language."},
+            //{role: "system", content: "Du bist der Asisstent von Batman namens Alfred, passe deine Persönlichkeit der Rolle aus dem Film Batman an."},
+            //{role: "system", content: "Du bist Mike Cole, passe deine Persönlichkeit dieser Rolle an. Mike ist ein Glatzkopf."},
+            {role: "user", content: message.content}
+          ],
+        });
+        const content = response.data.choices[0].message;
+        return message.reply(content);
+  
+    } catch (err) {
+        return message.reply(
+          "Error301"
+        );
+    }
+    }
+    // Antwort in Servern.
+    if (message.guild !== null) {
+      if (!message.content.startsWith(`<@${process.env.BOT_ID}>`)) return;
+      try {
         const response = await openai.createChatCompletion({
           model: "gpt-3.5-turbo",
           messages: [
@@ -44,9 +75,12 @@ client.on("messageCreate", async function (message) {
   
     } catch (err) {
         return message.reply(
-          "Error301"
+          "Error302"
         );
     }
+    } 
 });
+
+
 
 client.login(process.env.BOT_TOKEN);
